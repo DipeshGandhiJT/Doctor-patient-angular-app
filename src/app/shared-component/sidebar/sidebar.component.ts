fd083ef4a974b97ce975b28bb32c83b60e138f29
Dipesh Gandhi <dipesh_gandhi9@yahoo.com>
2022-10-08 09:37:54 +0530

    Add gitignore

--- src/app/shared-component/sidebar/sidebar.component.ts
+++ src/app/shared-component/sidebar/sidebar.component.ts
@@ -1,24 +1,26 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewEncapsulation,
-} from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[] = [];
   @Input() visibleSidebar: boolean = true;
-  constructor() {}
+  constructor(public translate: TranslateService) {}
 
   ngOnInit(): void {
-    this.menuItems = [{ index: 0, label: 'DASHBOARD', icon: 'pi pi-th-large' }];
+    this.menuItems = [
+      { index: 0, label: "DASHBOARD", icon: "pi pi-th-large" },
+      {
+        index: 1,
+        label: this.translate.instant("global.clients"),
+        icon: "pi pi-users",
+      },
+      { index: 2, label: "OPD", icon: "pi pi-th-large" },
+    ];
   }
 }
